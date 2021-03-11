71b3021ae5e2fabf22ff29413296ddbb8e2fe91a
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2021-03-11 19:56:15 -0300

    fatorial (#9)
    * fatorial
    
    * Update fatorial.js
    
    Exportando função

---
+++ fatorial.js
@@ -0,0 +1,12 @@
+export const fatorial = (num)=>{
+    const numero = num;
+    let resultado=1;
+    let count=1;
+
+    while(count<=numero){
+    resultado *= count;
+    count++;
+    }
+    console.log(resultado);
+    return resultado;
+}
